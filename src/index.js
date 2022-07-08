7eadbd3347e4745a77ddefb4f7a28ce788651e6c
rupesss-09 <rupesss09@gmail.com>
2022-07-08 00:00:19 -0500

    React project setup and more cleanups

--- src/index.js
+++ src/index.js
@@ -1,5 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 
+const element = <h1>React</h1>
+console.log(element)
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(<h1>Learning React !!!</h1>);
