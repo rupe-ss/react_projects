2e092eb866f11bfcf9f8eadaf982956d31ceca72
rupesss-09 <rupesss09@gmail.com>
2022-07-09 14:28:34 -0500

    Project part one mark up, rendering JSX element

--- src/index.js
+++ src/index.js
@@ -1,11 +1,23 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 
-const element = <h1>Learning React !!!</h1>
-console.log(element)
+const element = (
+    <div>
+        <img src = {require("./react-logo.png")} width = "40px" />
+        <h1>This is a react practice project</h1>
+        <ul>
+            <li>Was first released in 2013</li>
+            <li>Was originally created by Jordan Walke</li>
+            <li>Has well over 100K stars on GitHub</li>
+            <li>Is maintained by Facebook</li>
+            <li>Powers thousands of enterprise apps, including mobile apps</li>
+        </ul>
+    </div>
+)
+//console.log(element)
 
 //Thought experiment: use .append() instead of ReactDOM
-document.getElementById("root").append(JSON.stringify(element))
+//document.getElementById("root").append(JSON.stringify(element))
 
-// const root = ReactDOM.createRoot(document.getElementById('root'));
-// root.render(element);
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(element);
